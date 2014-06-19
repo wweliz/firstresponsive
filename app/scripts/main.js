9186f702105b74bf8a5a4bc678ddcbf1fbc0b57a
Whitney Williams <wweliz@gmail.com>
2014-06-19 13:50:01 -0400

    added better comments; fixed classname issue on catgif; set picturefill images to width 100%

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,10 +1,24 @@
-$(window).resize(function(){
-	if ($(window).width() <= 1080){	
-		$('.square').hide()
-	};
-    if ($(window).width() >= 1080){	
-		$('.square').show()
-	}	
+'use strict';
+
+// USING FITTEXT JQUERY PLUG-IN //////////////////////
+$('h1').fitText();
+
+// USING FITVIDS JQUERY PLUG-IN //////////////////////
+		//Note: In order for FitVids to work, your video 
+		//must be wrapped in a div tag
+$(document).ready(function(){
+    $('.myvideo').fitVids();
 });
 
+// SHOW AND HIDE USING WINDOW RESIZE /////////////////
+$(window).resize(function(){
+	if ($(window).width() <= 960){
+		$('.catvideo').hide();
+	}
+    if ($(window).width() >= 960){
+		$('.catvideo').show();
+	}
+});
+
+//////////////////////////////////////////////////////
 console.log('Main JS loaded!');
